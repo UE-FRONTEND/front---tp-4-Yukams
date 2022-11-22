86164e6121ce786275b36d992f5003b34972c3d5
Valentin Tahon <60988435+Yukams@users.noreply.github.com>
2022-11-22 09:54:39 +0100

    Resolves 2

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,18 +1,14 @@
 describe('Make sure our todo list app is working well', () => {
     it('Test that we can open a browser and load our app', () => {
         cy.visit("http://127.0.0.1:4000");
-        cy.wait(5000); // /!\ /!\ /!\ USELESS LINE HERE <---
-        // Cypress always wait before doing the next action
-        // Please, do not copy that line for the rest of the TP.
+        cy.wait(5000);
     })
     it(
-        'SHould clear input field after adding a new todo', () => {
+        'Should clear input field after adding a new todo', () => {
             cy.visit("http://127.0.0.1:4000")
-            let field = cy.get('#new-todo-field')
-            field.type('test')
-            let button = cy.get('#new-todo-button')
-            button.click()
-            field.should()
+            cy.get('#new-todo-field').type('test')
+            cy.get('#new-todo-button').click()
+            cy.get('#new-todo-field').should("have.value", "")
         }
     )
 })
